7b14a1b85d7e9bef0e6483777cd2f26d650359ac
kik87827 <kik87827@gmail.com>
2025-07-31 17:31:35 +0900

    인재정보 상세검색 pc 코딩완료

--- dist/assets/js/ui.js
+++ dist/assets/js/ui.js
@@ -870,6 +870,34 @@ function responThFunc(targets) {
   }
 }
 
+function responMaxFunc(targets) {
+  // 문자열이면 배열로 변환
+  const targetArray = typeof targets === "string" ? [targets] : targets;
+
+  const $elements = targetArray.map((selector) => $(selector)).filter(Boolean);
+
+  action(); // 초기 실행
+  $(window).on("resize", action); // 리사이즈 대응
+
+  function action() {
+    $elements.forEach(($fieldset) => {
+      $fieldset.each(function() {
+        const $thisTb = $(this);
+        const $thisThText = $thisTb.find(".cluster_thtext");
+        const $thisThCols = $thisTb.find(".th_cols");
+        let $thisMaxArray = [];
+        $thisThCols.css("flex-basis", "");
+
+        if ($(window).width() <= 1023) {}
+        $thisThText.each(function() {
+          $thisMaxArray.push($(this).outerWidth());
+        });
+        $thisThCols.css("flex-basis", Math.max.apply(null, $thisMaxArray));
+      });
+    });
+  }
+}
+
 function responFileLabelFunc(target) {
   const respond_fieldset_tb = $(target) || target;
   action();
